d5cc99b74fc7ee0fcb23004ca3e31848589562a9
pairing4good <pairing4good@gmail.com>
2022-10-27 07:37:19 -0500

    update tests and readme

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 The `index.html` file
-- should contain 2 radio buttons with labels and one of them selected by default
-- should contain 3 checkboxes with labels and one of them checked by default
-- should contain 1 input with a placeholder and that is marked as required
-- should contain 1 submit button
+- should tab to the age input first
+- should tab to the name input second
+- should tab to the city input third
 
 Resources:
-- https://www.w3schools.com/tags/tag_input.asp
-- https://www.w3schools.com/tags/tag_button.asp
+- https://www.w3schools.com/tags/att_global_tabindex.asp
--- test/verify.test.js
+++ test/verify.test.js
@@ -36,110 +36,25 @@ afterEach(async () => {
   await browser.close();
 });
 
-describe('form', () => {
-  it('should contain two radio buttons', async () => {
-    let radioButtons = await page.$$('form > input[type="radio"]');
-    expect(radioButtons.length).toBe(2);
+describe("the form", () => {
+  it("should tab to the age input first", async () => {
+    const tabindex = await page.$$eval('form > input[id="age"]', (inputs) => {
+      return inputs[0].getAttribute("tabindex");
+    });
+    expect(tabindex).toBe("1");
   });
-  
-  it('should contain three checkboxes', async () => {
-    let checkboxes = await page.$$('form > input[type="checkbox"]');
-    expect(checkboxes.length).toBe(3);
+
+  it("should tab to the name input second", async () => {
+    const tabindex = await page.$$eval('form > input[id="name"]', (inputs) => {
+      return inputs[0].getAttribute("tabindex");
+    });
+    expect(tabindex).toBe("2");
   });
-  
-  it('should contain one text input', async () => {
-    let textBoxes = await page.$$('form > input[type="text"]');
-    expect(textBoxes.length).toBe(1);
-  });
-  
-  it('should contain one submit button', async () => {
-    let submitButtons = await page.$$('form > button[type="submit"]');
-    expect(submitButtons.length).toBe(1);
+
+  it("should tab to the city input first", async () => {
+    const tabindex = await page.$$eval('form > input[id="city"]', (inputs) => {
+      return inputs[0].getAttribute("tabindex");
+    });
+    expect(tabindex).toBe("3");
   });
 });
-
-describe('radio buttons', () => {
-  
-  it('should have matching labels', async () => {
-    const ids = await page.$$eval(`form > input[type="radio"]`, (radioButtons) => {
-      let ids = [];
-      for(let i = 0; i < radioButtons.length; i++){
-        let radioButton = radioButtons[i];
-        const id = radioButton.getAttribute("id");
-        ids.push(id);
-      }
-      return ids;
-    });
-
-    expect(ids.length).toBe(2);
-
-    for(let i = 0; i < ids.length; i++){
-      const labels = await page.$$(`form > label[for='${ids[i]}']`);
-      expect(labels.length).toBe(1);
-    }
-  });
-
-  it('should have at least one selected by default', async () => {
-    const selected = await page.$$eval(`form > input[type="radio"]`, (radioButtons) => {
-      let selected = 0;
-      for(let i = 0; i < radioButtons.length; i++){
-        let radioButton = radioButtons[i];
-        
-        if(radioButton.hasAttribute("checked")) selected++;
-      }
-      return selected;
-    });
-    
-    expect(selected).toBeGreaterThan(0);
-  });
-});
-
-describe('checkboxes', () => {  
-  it('should have matching labels', async () => {
-    const ids = await page.$$eval('form > input[type="checkbox"]', (checkboxes) => {
-      let ids = [];
-      for(let i = 0; i < checkboxes.length; i++){
-        let checkbox = checkboxes[i];
-        const id = checkbox.getAttribute("id");
-        ids.push(id);
-      }
-      return ids;
-    });
-     
-    expect(ids.length).toBe(3);
-
-    for(let i = 0; i < ids.length; i++){
-      let label = await page.$$(`form > label[for='${ids[i]}']`); 
-      expect(label.length).toBe(1);
-    }
-  });
-  
-  it('should have at least one checked by default', async () => {
-    const selected = await page.$$eval(`form > input[type="checkbox"]`, (checkboxes) => {
-      let selected = 0;
-      for(let i = 0; i < checkboxes.length; i++){
-        let checkbox = checkboxes[i];
-        
-        if(checkbox.hasAttribute("checked")) selected++;
-      }
-      return selected;
-    });
-    expect(selected).toBeGreaterThan(0);
-  });
-});
-
-describe('text input', () => {
-  it('should have a placholder', async () => {
-    const answer = await page.$$eval('form > input[type="text"]', (textInputs) => {
-      return textInputs[0].hasAttribute('placeholder');
-    });
-    expect(answer).toBe(true);
-  })
-  
-  it('should be required', async () => {
-    const answer = await page.$$eval('form > input[type="text"]', (textInputs) => {
-      return textInputs[0].hasAttribute('required');
-    });
-    expect(answer).toBe(true);
-  })
-})
